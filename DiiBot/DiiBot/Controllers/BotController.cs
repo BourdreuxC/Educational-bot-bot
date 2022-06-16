90a5d20b106ddea75a96310ec8d6f7be28c7a9da
Nicolas Civade <mopigno@gmail.com>
2022-06-16 10:12:16 +0200

    Make styleCop

--- DiiBot/DiiBot/Controllers/BotController.cs
+++ DiiBot/DiiBot/Controllers/BotController.cs
@@ -1,38 +1,48 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
-// Generated with Bot Builder V4 SDK Template for Visual Studio EmptyBot v4.15.2
-
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using System.Threading.Tasks;
-
+﻿// <copyright file="BotController.cs" company="DiiBot">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 namespace DiiBot.Controllers
 {
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
     // This ASP Controller is created to handle a request. Dependency Injection will provide the Adapter and IBot
     // implementation at runtime. Multiple different IBot implementations running at different endpoints can be
     // achieved by specifying a more specific type for the bot constructor argument.
+
+    /// <summary>
+    /// Primary endpoint for the bot interactions.
+    /// </summary>
     [Route("api/messages")]
     [ApiController]
     public class BotController : ControllerBase
     {
-        private readonly IBotFrameworkHttpAdapter _adapter;
-        private readonly IBot _bot;
+        private readonly IBotFrameworkHttpAdapter adapter;
+        private readonly IBot bot;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BotController"/> class.
+        /// </summary>
+        /// <param name="adapter">Injection of the adapter that will determine the action.</param>
+        /// <param name="bot">Intance of the bot that will execure treatements.</param>
         public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
         {
-            _adapter = adapter;
-            _bot = bot;
+            this.adapter = adapter;
+            this.bot = bot;
         }
 
+        /// <summary>
+        /// Delegate the processing of the HTTP POST to the adapter.
+        /// The adapter will invoke the bot.
+        /// </summary>
+        /// <returns>null.</returns>
         [HttpPost]
         [HttpGet]
         public async Task PostAsync()
         {
-            // Delegate the processing of the HTTP POST to the adapter.
-            // The adapter will invoke the bot.
-            await _adapter.ProcessAsync(Request, Response, _bot);
+            await this.adapter.ProcessAsync(this.Request, this.Response, this.bot);
         }
     }
 }
--- DiiBot/DiiBot/Program.cs
+++ DiiBot/DiiBot/Program.cs
@@ -1,20 +1,31 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
-// Generated with Bot Builder V4 SDK Template for Visual Studio EmptyBot v4.15.2
-
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿// <copyright file="Program.cs" company="DiiBot">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace DiiBot
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    /// <summary>
+    /// Instanciate the app.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Primaty method.
+        /// </summary>
+        /// <param name="args">Array of arguments used in order to configure the app.</param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Create an hosting environement to handle queries.
+        /// </summary>
+        /// <param name="args">Array of arguments used in order to configure the app.</param>
+        /// <returns>IHostBuilder.</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
